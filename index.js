ca97889ac44cc1dca89d0ec168f780b78702c726
Arden007 <cloveryuno664@gmail.com>
2022-03-23 16:27:16 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -17,19 +18,18 @@ mongoose
     console.log(err);
   });
 
-  app.use(express.json());
-    // USER
-  app.use("/user", userRouter);
-  // AUTH
-  app.use("/auth", authRouter);
-  // PRODUCT
-  app.use("/product", productRouter);
-  // Cart
-  app.use("/cart", cartRouter);
-  // Order
-  app.use("/order", orderRouter);
-
-
+app.use(express.json());
+app.use(Cors());
+// USER
+app.use("/user", userRouter);
+// AUTH
+app.use("/auth", authRouter);
+// PRODUCT
+app.use("/product", productRouter);
+// Cart
+app.use("/cart", cartRouter);
+// Order
+app.use("/order", orderRouter);
 
 app.listen(process.env.PORT || 0808, () => {
   console.log("Backend Server is running!");
