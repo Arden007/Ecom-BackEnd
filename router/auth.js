3385b6a68f3001c51e004dfdbed916a6e4185a22
Arden007 <cloveryuno664@gmail.com>
2022-03-25 09:04:37 +0200

    update

--- router/auth.js
+++ router/auth.js
@@ -2,9 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
-const {
-  authorizationToken
-} = require("./verifyToken");
+const { authorizationToken } = require("./verifyToken");
 
 // REGISTER
 router.post("/register", async (req, res) => {
@@ -26,10 +24,10 @@ router.post("/register", async (req, res) => {
 });
 
 // LOGIN
-router.post("/login",authorizationToken, async (req, res) => {
+router.patch("/login", authorizationToken, async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(401).json("Wrong Credentials!")
+    !user && res.status(401).json("Wrong Credentials!");
 
     const hasPassword = CryptoJS.AES.decrypt(
       user.password,
@@ -40,20 +38,20 @@ router.post("/login",authorizationToken, async (req, res) => {
     OriginalPassword !== req.body.password &&
       res.status(401).json("Wrong Credentials!");
     // we gonna create an accessToken that will check if our user is an admin or if what they want to edit belongs to them
-      const accessToken = jwt.sign(
-        {
-          id: user._id,
-          isAdmin: user.isAdmin,
-        },
-        process.env.JWT_SEC,
-    //   the token will expire after 3 days and the user will have to login again
-        {expiresIn:"3d"}
-        );
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      //   the token will expire after 3 days and the user will have to login again
+      { expiresIn: "3d" }
+    );
 
-    // I will be using a spread operator to prevent the user password from displaying 
-     const {password, ...others } = user._doc;
+    // I will be using a spread operator to prevent the user password from displaying
+    const { password, ...others } = user._doc;
     // mongoDB stores our data in a _doc file so to show only the info we want and not the path in which its stored, to do this just add _doc to user
-      res.status(200).json({...others, accessToken}); 
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
